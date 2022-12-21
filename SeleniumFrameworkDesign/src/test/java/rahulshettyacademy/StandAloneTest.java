e8d8449aae9b8142f5b43b6856863833a4c9e240
kashidrahul <kashidrahulavp@gmail.com>
2022-12-21 15:38:39 +0530

    selenium framework

---
+++ SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/StandAloneTest.java
@@ -0,0 +1,62 @@
+package rahulshettyacademy;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class StandAloneTest 
+{
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		String productName = "ZARA COAT 3";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.get("https://rahulshettyacademy.com/client");
+
+		driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys("kashidrahulavp@gmail.com");
+		driver.findElement(By.xpath("//input[@id='userPassword']")).sendKeys("Rahul@15");
+		driver.findElement(By.xpath("//input[@id='login']")).click();
+		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+
+		//		driver.findElement(By.xpath("(//button[@class='btn w-10 rounded'])[1]")).click();
+
+		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+		WebElement prod = products.stream().filter(product->
+		product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst().orElse(null);
+		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		//ng-animating
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+		Boolean match = cartProducts.stream().anyMatch(cartProduct-> cartProduct.getText().equalsIgnoreCase(productName));
+		Assert.assertTrue(match);
+		
+		driver.findElement(By.cssSelector(".totalRow button")).click();
+		Actions a = new Actions(driver);
+		a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")),"India").build().perform();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+		
+		driver.findElement(By.xpath("(//button[contains(@class,'ta-item')])[2]")).click();
+		driver.findElement(By.xpath("//a[@class='btnn action__submit ng-star-inserted']")).click();
+		
+		String confirmMessage = driver.findElement(By.xpath("//h1[contains (text(),'Thankyou for the order.')]")).getText();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR YOUR ORDER."));
+		driver.close();		
+	}
+
+}
